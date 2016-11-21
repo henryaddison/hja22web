7b299a2e1d082ea81a8eaf06689c05c690975e58
Henry Addison <hja22@hja22.co.uk>
2016-11-21 20:54:41 +0000

    use Handlebars templates to display top-site links

---
+++ app.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+  var links = [
+    {
+      href: "https://www.bbc.co.uk/news",
+      favicon_url: "https://www.bbc.co.uk/favicon.ico",
+      text: "BBC News"
+    },{
+      href: "https://www.theguardian.com/uk",
+      favicon_url: "https://www.theguardian.com/favicon.ico",
+      text: "The Guardian"
+    },{
+      href:"https://arstechnica.co.uk",
+      favicon_url: "https://arstechnica.co.uk/favicon.ico",
+      text: "Ars Technica"
+    },{
+      href: "http://www.espncricinfo.com",
+      favicon_url: "http://www.espncricinfo.com/favicon.ico",
+      text: "Cricinfo"
+    },{
+      href: "https://news.ycombinator.com/news",
+      favicon_url: "https://news.ycombinator.com/favicon.ico",
+      text: "Hacker News"
+    },{
+      href: "http://www.datatau.com/news",
+      favicon_url: "http://www.datatau.com/favicon.ico",
+      text: "Data Tau"
+    },{
+      href: "https://feedly.com/index.html",
+      favicon_url: "https://feedly.com/favicon.ico",
+      text: "Feedly"
+    }
+  ]
+
+  Handlebars.registerPartial("link", $("#linkPartial").html());
+  var template = Handlebars.compile($("#linksTemplate").html());
+  var results = template({links: links});
+  $("#top-sites").html(results);
+});
